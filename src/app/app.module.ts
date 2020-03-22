a12b734f52b79f75037ce8b0cb5a6da6258915f7
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-03-22 15:18:49 -0400

    created inital components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './routes/home/home/home.component';
 import { ChannelComponent } from './channel/channel.component';
 import { WatchComponent } from './watch/watch.component';
+import { ThumbnailComponent } from './core/components/thumbnails/thumbnail/thumbnail.component';
+import { CommentsComponent } from './core/components/comments/comments/comments.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ChannelComponent,
-    WatchComponent
+    WatchComponent,
+    ThumbnailComponent,
+    CommentsComponent
   ],
   imports: [
     BrowserModule,
