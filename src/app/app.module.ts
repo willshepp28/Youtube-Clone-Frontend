a6930b1818ee88d411d677931cabf4947888b470
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-03-22 15:17:05 -0400

    setup inital routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './routes/home/home/home.component';
+import { ChannelComponent } from './channel/channel.component';
+import { WatchComponent } from './watch/watch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ChannelComponent,
+    WatchComponent
   ],
   imports: [
     BrowserModule,
