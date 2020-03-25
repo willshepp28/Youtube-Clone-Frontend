3e87f9ee47c4c5db75512eb4e30ed46ac3ba83d6
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-03-25 19:21:56 -0400

    changed app prefix name; added logo and video-camera icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './routes/home/home/home.component';
-import { ChannelComponent } from './channel/channel.component';
+import { ChannelComponent } from './routes/channel/channel.component';
 import { WatchComponent } from './watch/watch.component';
 import { ThumbnailComponent } from './core/components/thumbnails/thumbnail/thumbnail.component';
 import { CommentsComponent } from './core/components/comments/comments/comments.component';
 import { HeaderComponent } from './core/components/header/header/header.component';
+import { LogoComponent } from './core/components/icons/logo/logo/logo.component';
+import { VideoCameraComponent } from './core/components/icons/video-camera/video-camera/video-camera.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,9 @@ import { HeaderComponent } from './core/components/header/header/header.componen
     WatchComponent,
     ThumbnailComponent,
     CommentsComponent,
-    HeaderComponent
+    HeaderComponent,
+    LogoComponent,
+    VideoCameraComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/core/components/icons/video-camera/video-camera/video-camera.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'youtube-video-camera',
+  templateUrl: './video-camera.component.html',
+  styleUrls: ['./video-camera.component.css']
+})
+export class VideoCameraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
